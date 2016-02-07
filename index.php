9973773e53a32466cd9f7cbb0f886888cf6901ef
a.semenov <star87.ubs@gmail.com>
2016-02-07 09:22:15 +0300

    my_first_commit

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-
-<<!DOCTYPE html>
 <html>
 <head>
 	<title>Virtual server number one</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<div id="page">
-		
+		<div id="header">Шапка</div>
+		<div id="content">Контент</div>
+		<div id="footer">Подвал</div>
 	</div>
 </body>
 </html>
